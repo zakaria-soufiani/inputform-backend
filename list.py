8c6a4cfdee0fd76731047f01da4329c7cb4704a7
zak <mz.soufiani@gmail.com>
2017-05-12 13:10:00 +1000

    .yml update

--- list.py
+++ list.py
@@ -12,7 +12,7 @@ dynamodb = boto3.resource('dynamodb')
 def list(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
-    # fetch all todos from the database
+    # fetch all entries from the database
     result = table.scan()
 
     # create a response
